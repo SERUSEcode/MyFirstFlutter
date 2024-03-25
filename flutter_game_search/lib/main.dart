664f4a1e1e10f6c970908e28920af2a0e6334628
SERUSEcode <kevin@ullbrandt.com>
2024-03-25 14:58:30 +0100

    project now can read json file and output content

--- flutter_game_search/lib/main.dart
+++ flutter_game_search/lib/main.dart
@@ -63,9 +63,10 @@ class _MyHomePageState extends State<MyHomePage> {
   List _items = [];
 
   Future<void> readJson() async {
-    final String response = await rootBundle.loadString('assets/test.json');
+    final String response = await rootBundle.loadString('assets/sample.json');
     final data = await json.decode(response);
     setState(() {
+      print("hi");
       _items = data["items"];
       print(".. number of items ${_items.length}");
     });
@@ -81,31 +82,29 @@ class _MyHomePageState extends State<MyHomePage> {
           'dbestech',
         ),
       ),
-      body:Column(
-        children: [
-          _items.isNotEmpty?Expanded(
-            child: ListView.builder(
-              itemCount: _items.length,
-              itemBuilder: (context, index) {
-                return Card(
-                  key: ValueKey(_items[index]["name"]),
-                  margin: const EdgeInsets.all(10),
-                  color: Colors.amber.shade100,
-                  child: ListTile(
-                    leading: Text(_items[index]["name"]),
-                    title: Text(_items[index]["name"]),
-                    subtitle: Text(_items[index]["price"]),
-                  ),
-                );
-              },
-            ), 
-        ): ElevatedButton(
-          onPressed: () {
+      body:Column(children: [
+        _items.isNotEmpty?Expanded(
+        child: ListView.builder(
+          itemCount: _items.length,
+          itemBuilder: (context, index) {
+            return Card(
+              key: ValueKey(_items[index]["id"]),
+              margin: const EdgeInsets.all(10),
+              color: Colors.amber.shade100,
+              child: ListTile(
+                leading: Text(_items[index]["id"]),
+                title: Text(_items[index]["name"]),
+                subtitle: Text(_items[index]["description"]),
+              ),
+            );
+          },
+        ),
+      ): ElevatedButton(
+        onPressed: () {
           readJson();
         },
         child: Center(child: Text("Load Json")))
-        ],
-      ),
+      ],),
     );
   }
 }
